91697c161da5ea76d68a6fe77a031f153e37391a
Matt Davidson <matthewdavidson12@gmail.com>
2015-11-21 14:49:50 +0000

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -18,22 +20,37 @@ class NewVisitorTest(unittest.TestCase):
 
         # she notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # she is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
-        # she types "Buy peacock feathers" into a text box 
+        # she types "Buy peacock feathers" into a text box
+        inputbox.send_keys('Buy peacock feathers')
 
         # when she hits enter, the page updates, and now the page lists
         # "Buy peacock feathers" into a text box" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
 
-        # There is still a text box invited her to add another item. She 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tage_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        # There is still a text box invited her to add another item. She
         # enters "Use peacock feathers to make a fly"
+        self.fail('Finish the test!')
 
         # the page updates again and now shoes both items on her list
 
         # edith wonders whether the site will remember he r list. The she sees
-        # that the site has generated a unique URL for her -- there is some 
+        # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect
 
         # she visits that URL - her to-do list is still there.
